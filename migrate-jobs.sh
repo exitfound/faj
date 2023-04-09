7f09d321e546da65b5697178287a23116bd08386
medoed <medoed@kitezh.online>
2023-04-09 16:51:57 +0300

    Added ability to do import export jobs with spaces in name

--- migrate-jobs.sh
+++ migrate-jobs.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-help()
-{
-  echo "        [ -o | --old-jenkins ]          Specify the address of the Old Jenkins from which you want to export jobs.
-        [ -n | --new-jenkins ]          Specify the address of the Old Jenkins where you want to import the jobs.
-        [ -u | --old-jenkins-user ]     Specify a user in Old Jenkins to access jobs.
-        [ -p | --old-jenkins-password ] Specify a password for user in Old Jenkins to access jobs.
-        [ -U | --new-jenkins-user ]     Specify a user in New Jenkins to upload jobs.
-        [ -P | --new-jenkins-password ] Specify a password for user in New Jenkins to upload jobs.
-        [ -h | --help ]                 General information about all options."
-  exit 2
-}
-
-DIRECTORY=jobs
-LIST_JOBS=out.txt
+DIRECTORY="jobs"
+LIST_JOBS="./jobs/out.txt"
 JENKINS_CLI=jenkins-cli.jar
 SHORT=o:,n:,u:,p:,U:,P:,h
 LONG=old-jenkins:,new-jenkins:,old-jenkins-user:,old-jenkins-password:,new-jenkins-user:,new-jenkins-password:,help
 OPTS=$(getopt -a -n jenkins --options $SHORT --longoptions $LONG -- "$@")
 
 if [ $? -ne 0 ]; 
-  then echo "ERROR: You are using one of the invalid parameters."; 
+  then echo -e "ERROR: You are using one of the invalid parameters! \n"; 
   exit 1; 
 fi
+
+help()
+{
+  echo -e "These are the possible options to use: \n"
+  echo -e "        [ -o | --old-jenkins ]          Specify the address of the Old Jenkins from which you want to export jobs.
+        [ -n | --new-jenkins ]          Specify the address of the Old Jenkins where you want to import the jobs.
+        [ -u | --old-jenkins-user ]     Specify a user in Old Jenkins to access jobs.
+        [ -p | --old-jenkins-password ] Specify a password for user in Old Jenkins to access jobs.
+        [ -U | --new-jenkins-user ]     Specify a user in New Jenkins to upload jobs.
+        [ -P | --new-jenkins-password ] Specify a password for user in New Jenkins to upload jobs.
+        [ -h | --help ]                 General information about all options. \n"
+  echo -e "Usage: ./migrate-jobs.sh [OPTIONS] [ARGS]"
+  exit 2
+}
+
+prepare()
+{
+  rm -rf ${DIRECTORY}
+  mkdir -p ${DIRECTORY}
+}
 
 eval set -- "$OPTS"
 
@@ -54,8 +62,6 @@ do
       shift 2
       ;;
     -h | --help)
-      echo -e '\n'
-      echo "These are the possible options to use:"
       help
       ;;
     --)
@@ -65,29 +71,23 @@ do
   esac
 done
 
-if [ -d "$DIRECTORY" ]; then
-    echo -e "Directory $DIRECTORY already exists: \n"
-else
-	mkdir -p $DIRECTORY
-fi
+prepare
 
 if [ ! -e "$JENKINS_CLI" ]; then
     echo -e "Downloads jenkins cli to successfully apply the script: \n"
-    wget $OLD_JENKINS/jnlpJars/jenkins-cli.jar
-else 
+    wget "$OLD_JENKINS"/jnlpJars/jenkins-cli.jar
+else
     echo -e "Jenkins cli already exists on your path: \n"
 fi
 
-for i in $(java -jar jenkins-cli.jar -s $OLD_JENKINS -auth $OLD_JENKINS_LOGIN:$OLD_JENKINS_PASSWORD list-jobs);
-do 
-  java -jar jenkins-cli.jar -s $OLD_JENKINS -auth $OLD_JENKINS_LOGIN:$OLD_JENKINS_PASSWORD get-job ${i} > ./jobs/${i};
-done
+java -jar jenkins-cli.jar -s "$OLD_JENKINS" -auth "$OLD_JENKINS_LOGIN":"$OLD_JENKINS_PASSWORD" list-jobs > "$LIST_JOBS"
 
-ls $DIRECTORY > $LIST_JOBS
+while IFS= read -r line <&3; do
+  java -jar jenkins-cli.jar -s "$OLD_JENKINS" -auth "$OLD_JENKINS_LOGIN":"$OLD_JENKINS_PASSWORD" get-job "$line" > ./"$DIRECTORY"/"$line";
+done 3<"$LIST_JOBS"
 
-for e in $(cat $LIST_JOBS);
-do 
-  java -jar jenkins-cli.jar -s $NEW_JENKINS -auth $NEW_JENKINS_LOGIN:$NEW_JENKINS_PASSWORD create-job ${e} < ./jobs/${e};
-done
+while IFS= read -r line <&3; do
+  java -jar jenkins-cli.jar -s "$NEW_JENKINS" -auth "$NEW_JENKINS_LOGIN":"$NEW_JENKINS_PASSWORD" create-job "$line" < ./"$DIRECTORY"/"$line";
+done 3<"$LIST_JOBS"
 
-rm -rf $LIST_JOBS $DIRECTORY
+rm -rf "$DIRECTORY"
